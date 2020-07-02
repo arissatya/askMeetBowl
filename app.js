2d652d76533abcc7f8ced72748e338946639e9e1
ArisSatya <arissatya.nugraha@gmail.com>
2020-07-02 13:09:36 +0700

    new tweak

---
+++ app.js
@@ -0,0 +1,12 @@
+let bakso = [
+    'Bakso Solo',
+    'Bakwan Malang',
+    'Bakso Wonogiri',
+    'Baso Cuanki',
+    'Mie Bakso',
+    'Baso Jumbo',
+    'Baso Tusuk',
+    'Baso Aci',
+    'Bakso ikan',
+]
+
