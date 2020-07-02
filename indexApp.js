7752984b461ac767971c52952ff4985f7606462c
ArisSatya <arissatya.nugraha@gmail.com>
2020-07-03 00:35:02 +0700

    minor

--- indexApp.js
+++ indexApp.js
@@ -41,17 +41,17 @@ var slideIndex = 0;
 showSlides();
 
 function showSlides() {
-    // var i;
     var slides = document.getElementsByClassName("mySlides");
     var dots = document.getElementsByClassName("dot");
         for (let i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";  
         }
         slideIndex++;
-        if (slideIndex > slides.length) {slideIndex = 1}    
-            for (let i = 0; i < dots.length; i++) {
-            dots[i].className = dots[i].className.replace(" active", "");
-            }
+        if (slideIndex > slides.length){
+            slideIndex = 1}    
+        for (let i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+        }
     slides[slideIndex-1].style.display = "block";  
     dots[slideIndex-1].className += " active";
     setTimeout(showSlides, 6000);
