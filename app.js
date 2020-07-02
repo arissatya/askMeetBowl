2da3c8ba567d1f609d6e238b4ec3110e4e583bad
ArisSatya <arissatya.nugraha@gmail.com>
2020-07-02 14:27:47 +0700

    its working

--- app.js
+++ app.js
@@ -17,7 +17,12 @@ let click = document.getElementById("click")
 
 click.addEventListener("click", function(){
     // console.log('tes');
-    let urutan = Math.floor(Math.random()*bakso.length)
     // console.log(click.innerText);
+    let urutan = Math.floor(Math.random()*bakso.length)
     click.innerText = bakso[urutan]
+    window.alert(`kamu mau makan ${bakso[urutan]} ga?`);
 })
+
+// click.addEventListener("click", function(){
+//     let urutan = Math.floor(Math.random()*bakso.length)
+// })
