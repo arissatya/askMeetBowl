00f27ff1398cf84f24427461696eb72981956d48
ArisSatya <arissatya.nugraha@gmail.com>
2020-07-02 13:41:49 +0700

    its alive!

--- app.js
+++ app.js
@@ -7,6 +7,17 @@ let bakso = [
     'Baso Jumbo',
     'Baso Tusuk',
     'Baso Aci',
-    'Bakso ikan',
+    'Bakso Ikan',
+    'Bakso Spagheti',
+    'Baso Cilok',
 ]
 
+let click = document.getElementById("click")
+// let showBakso = document.getElementsByClassName("intro")
+
+click.addEventListener("click", function(){
+    // console.log('tes');
+    let urutan = Math.floor(Math.random()*bakso.length)
+    // console.log(click.innerText);
+    click.innerText = bakso[urutan]
+})
