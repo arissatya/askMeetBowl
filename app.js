77e2fa687dda10d5b72c3879e0e6e153826b71d9
ArisSatya <arissatya.nugraha@gmail.com>
2020-07-02 20:28:37 +0700

    major uppgit add .

--- app.js
+++ app.js
@@ -35,6 +35,24 @@ click.addEventListener("click", function(){
     window.alert(`${minuman[minum]}`);
 })
 
-// click.addEventListener("click", function(){
-//     let urutan = Math.floor(Math.random()*bakso.length)
-// })
+//uji coba slideshow
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    // var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+        for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+        }
+        slideIndex++;
+        if (slideIndex > slides.length) {slideIndex = 1}    
+            for (let i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" active", "");
+            }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 6000);
+}
